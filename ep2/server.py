89963065ecf2470b15800b003a308943b28bcad3
Renan Silva <renan.tssilva@gmail.com>
2019-10-11 16:49:00 -0300

    begin ep2

---
+++ ep2/server.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+import socket
+import sys
+import time
+import network
+
+HOST = '0.0.0.0'
+PORT = 63234
+
+def client():
+    print('client')
+
+def server():
+    print('server')
+    ip = network.my_ip()
+    print(ip)
+
+def main():
+    if(len(sys.argv) == 1):
+        client()
+    elif(len(sys.argv) == 2):
+        server()
+    else:
+        print('Numero invalido de argumentos')
+
+if __name__ == "__main__":
+    main()
